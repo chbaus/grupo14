dba59c760df855651a15ca7afd5490d056ba76f1
Christian <christianbaus@hotmail.com>
2022-02-23 15:55:23 -0300

    primer commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const btnSaludo = document.getElementById("btnSaludo");
+
+btnSaludo.addEventListener("click", ()=>{
+    alert("Esto es js avanzado");
+});
+
+function despedir(){
+    alert("Chau mundo");
+}
